c8aaf28c16821836aede281d05c41ccac5888970
kennkash <kkashmiry23@gmail.com>
2026-04-01 12:26:44 -0500

    Create admin-index.ts

---
+++ admin-index.ts
@@ -0,0 +1,11 @@
+// /rail-at-sas/frontend/components/admin/index.ts
+/**
+ * Admin Components Index
+ * Export all admin-related components
+ */
+
+export { AdminConfigPage } from "./admin-config-page";
+export { GeneralSettingsTab } from "./general-settings-tab";
+export { RestrictedProjectsTab } from "./restricted-projects-tab";
+export { EchoAiSettingsTab } from "./echo-ai-settings-tab";
+
